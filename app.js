bfa6baada423d277bd0dd9e6b04c99685ebb37e7
Kim Damsleth <kim@damsleth.com>
2018-02-08 16:49:55 +0100

    switching bots - moved to cosmosfb

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var restify = require('restify');
 var builder = require('botbuilder');
-var azure = require('botbuilder-azure');
+var azure = require('azure-storage');
+var botbuilder_azure = require("botbuilder-azure");
 require('dotenv').config();
 var dbURI = process.env.documentDBURI
 var dbKey = process.env.documentDBMasterKey
@@ -15,6 +16,7 @@ var documentDbOptions = {
 var docDbClient = new azure.DocumentDbClient(documentDbOptions);
 var cosmosStorage = new azure.AzureBotStorage({ gzipData: false }, docDbClient);
 
+
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
@@ -24,13 +26,58 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword
+    appPassword: process.env.MicrosoftAppPassword,
+    openIdMetadata: process.env.BotOpenIdMetadata
 });
 
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
-}).set('storage', cosmosStorage);
+// We're running CosmosDB DocumentDbClient here, so no azure tables. 
+// Leaving for posterity
+// var tableName = 'botdata';
+// var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
+// var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+
+// Create your bot with a function to receive messages from the user
+var bot = new builder.UniversalBot(connector);
+bot.set('storage', cosmosStorage);
+
+// Intercept trigger event (ActivityTypes.Trigger)
+bot.on('trigger', function (message) {
+    // handle message from trigger function
+    var queuedMessage = message.value;
+    var reply = new builder.Message()
+        .address(queuedMessage.address)
+        .text('I have requested an army for your war, sire');
+    //.text('This is coming from the trigger: ' + queuedMessage.text);
+    bot.send(reply);
+});
+
+// Handle message from user
+bot.dialog('/', function (session) {
+    var queuedMessage = { address: session.message.address, text: session.message.text };
+    // add message to queue
+    session.sendTyping();
+    var queueSvc = azure.createQueueService(process.env.AzureWebJobsStorage);
+    queueSvc.createQueueIfNotExists('bot-queue', function (err, result, response) {
+        if (!err) {
+            // Add the message to the queue
+            var queueMessageBuffer = new Buffer(JSON.stringify(queuedMessage)).toString('base64');
+            queueSvc.createMessage('bot-queue', queueMessageBuffer, function (err, result, response) {
+                if (!err) {
+                    // Message inserted
+                    //   session.send('Your message (\'' + session.message.text + '\') has been added to a queue, and it will be sent back to you via a Function');
+                    session.send('Yes my liege - I will summon an army and provision a war room for thee!');
+                } else {
+                    // this should be a log for the dev, not a message to the user
+                    session.send('There was an error inserting your message into queue');
+                }
+            });
+        } else {
+            // this should be a log for the dev, not a message to the user
+            session.send('There was an error creating your queue');
+        }
+    });
+
+});
