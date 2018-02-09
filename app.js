74f86552a594499f503943f512b810e9642f3559
Kim Damsleth <kim@damsleth.com>
2018-02-09 16:02:00 +0100

    GoToWar

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var restify = require('restify');
 var builder = require('botbuilder');
 var azure = require('azure-storage');
 var botbuilder_azure = require("botbuilder-azure");
-var dbURI = process.env.documentDBURI
-var dbKey = process.env.documentDBMasterKey
+var fetch = require('node-fetch');
+var dbURI = process.env.documentDBURI;
+var dbKey = process.env.documentDBMasterKey;
 
 var documentDbOptions = {
     host: dbURI,
@@ -37,7 +38,7 @@ var tableName = 'botdata';
 var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
 var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
 
-// we don't need to store all conversations in cosmosdb
+// we don't need to store all conversations in cosmosdb - OR DO WE??
 var inMemoryStorage = new builder.MemoryBotStorage();
 
 // Create your bot with a function to receive messages from the user
@@ -59,21 +60,14 @@ bot.on('trigger', function (message) {
     var queuedMessage = message.value;
     var reply = new builder.Message()
         .address(queuedMessage.address)
-        .text(queuedMessage.text);
-    // .text('This is coming from the trigger: ' + queuedMessage.text);
+        //    .text(queuedMessage.text);
+        .text('This is coming from the trigger: ' + queuedMessage.text);
     bot.send(reply);
 });
 
 
-bot.dialog('/', function (session) {
-    session.send("Yes, my liege?")
-})
-
-
-
-
 // START WAR DIALOG
-bot.dialog('war', function (session, args, next) {
+bot.dialog('war', (session) => {
     session.send("War, my liege?");
     session.beginDialog('startWar');
 }).triggerAction({ matches: /war$/i, });
@@ -96,51 +90,59 @@ bot.dialog('startWar', [
         session.dialogData.dragonsInWar = results.response;
         session.send("Excellent, my Lord.");
         session.send(`We will be declaring war upon house ${session.dialogData.houseToWageWarWith}, using ${session.dialogData.soldiersInWar} soldiers and ${session.dialogData.dragonsInWar} dragons.`)
-        session.send("I will send a raven immediately, to provision a war room for the forthcoming battle")
-        // TODO:
-        // SEND session.dialogData to flow azure function
+        session.send("I will send a raven immediately, to provision a war room for the forthcoming battle");
+
+        var flowUrl = "https://prod-53.westeurope.logic.azure.com:443/workflows/fab679e858a6476d91fc4fc048c98d61/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=jU2sWL379pWniGLgckcqsNLxVSfXi236zfltjJVyNbk"
+        var postBody = {
+            "Name": `The war against House ${session.dialogData.houseToWageWarWith}`,
+            "Description": `We will slay the ${session.dialogData.houseToWageWarWith}s, with ${session.dialogData.soldiersInWar} and ${session.dialogData.dragonsInWar}`,
+            "Responsible": "i:0#.f|membership|tarald@acdc1806.onmicrosoft.com",
+            "Public": "True",
+            "HouseName": "Cloudborne"
+        };
+        fetch(flowUrl, {
+            method: "POST",
+            body: JSON.stringify(postBody),
+            headers: { "content-type": "application/json" },
+        }).then(res => { session.send("Raven away, my Liege!") })
+
         session.endDialogWithResult(results);
     }
 ])
 
-
-// DICE POC
-bot.dialog('dice', function (session, args, next) {
-    var queuedMessage = { address: session.message.address, text: session.message.text };
-    // add message to queue
-    session.sendTyping();
-    var queueSvc = azure.createQueueService(process.env.AzureWebJobsStorage);
-    queueSvc.createQueueIfNotExists('dice-throw', function (err, result, response) {
-        if (!err) {
-            // Add the message to the queue
-            var queueMessageBuffer = new Buffer(JSON.stringify(queuedMessage)).toString('base64');
-            queueSvc.createMessage('dice-throw', queueMessageBuffer, function (err, result, response) {
-                if (!err) {
-                    session.send('Throwing a dice, my liege!');
-                } else { session.send('There was an error inserting your message into queue'); }
-            });
-        } else { session.send('There was an error creating your queue'); }
-    });
-}).triggerAction({ matches: /^dice$/i, });
-
-
 // LUIS DIALOG
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+
 var intents = new builder.IntentDialog({ recognizers: [recognizer] })
     .matches('Greeting', (session) => {
-        session.send('You reached Greeting intent, you said \'%s\'.', session.message.text);
+        var nowHours = new Date().getHours();
+        var timeOfDay = "day"
+        if (nowHours >= 0 && nowHours < 5) { timeOfDay = "night" } else
+            if (nowHours > 5 && nowHours < 10) { timeOfDay = "morning" } else
+                if (nowHours > 13 && nowHours < 17) { timeOfDay = "afternoon" } else
+                    if (nowHours > 17 && nowHours < 23) { timeOfDay = "evening" }
+        session.send(`Good ${timeOfDay}, my Liege!`);
     })
-    .matches('Help', (session) => {
-        session.send('You reached Help intent, you said \'%s\'.', session.message.text);
+    .matches('GoToWar', (session) => { session.send("War, my liege?"); session.beginDialog('startWar'); })
+    .matches('Help', (session) => { session.send("How may I help you, my liege?") })
+    .matches('Goodbye', (session) => { session.send("Farewell, my Liege") })
+    .matches('Cancel', (session) => { session.send('Sorry, Liege, what do you mean by \'%s\'.', session.message.text) })
+    .matches('ThrowDice', (session) => {
+        var queuedMessage = { address: session.message.address, text: session.message.text };
+        session.sendTyping();
+        var queueSvc = azure.createQueueService(process.env.AzureWebJobsStorage);
+        queueSvc.createQueueIfNotExists('dice-throw', function (err, result, response) {
+            if (!err) {
+                var queueMessageBuffer = new Buffer(JSON.stringify(queuedMessage)).toString('base64');
+                queueSvc.createMessage('dice-throw', queueMessageBuffer, function (err, result, response) {
+                    if (!err) { session.send('Throwing a dice, my liege!'); }
+                    else { session.send('There was an error inserting your message into queue'); }
+                });
+            } else { session.send('There was an error creating your queue'); }
+        });
     })
-    .matches('Cancel', (session) => {
-        session.send('You reached Cancel intent, you said \'%s\'.', session.message.text);
-    })
-    /*
-    .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
-    */
     .onDefault((session) => {
-        session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+        session.send("Forgive me, my liege, I did not understand...");
     });
 
 bot.dialog('/', intents);    
