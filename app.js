b8da5dad1a808b1991664f443ef26d82ff0ec86e
Kim Damsleth <kim@damsleth.com>
2018-02-09 01:59:51 +0100

    bot logic

--- app.js
+++ app.js
@@ -38,9 +38,12 @@ var tableName = 'botdata';
 var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
 var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
 
+// we don't need to store all conversations in cosmosdb
+var inMemoryStorage = new builder.MemoryBotStorage();
+
 // Create your bot with a function to receive messages from the user
 var bot = new builder.UniversalBot(connector);
-bot.set('storage', cosmosStorage);
+bot.set('storage', inMemoryStorage);
 
 // Intercept trigger event (ActivityTypes.Trigger)
 bot.on('trigger', function (message) {
@@ -48,35 +51,69 @@ bot.on('trigger', function (message) {
     var queuedMessage = message.value;
     var reply = new builder.Message()
         .address(queuedMessage.address)
-        .text('I have requested an army for your war, sire');
-    //.text('This is coming from the trigger: ' + queuedMessage.text);
-    bot.send(reply);
+        .text(queuedMessage.text);
+    // .text('This is coming from the trigger: ' + queuedMessage.text);
+        bot.send(reply);
 });
 
-// Handle message from user
-bot.dialog('/', function (session) {
-    var queuedMessage = { address: session.message.address, text: session.message.text };
+
+bot.dialog('/',function(session){
+    session.send("Yes, my liege?")
+})
+
+
+//CUSTOM
+
+
+
+// START WAR DIALOG
+bot.dialog('war', function (session, args, next) {
+    session.send("War, my liege?");
+    session.beginDialog('startWar');
+}).triggerAction({ matches: /war$/i, });
+
+// Prompt for a house to start war with
+bot.dialog('startWar', [
+    function (session) {
+        session.send("Very well...")
+        builder.Prompts.text(session, "Upon whom shall we declare war, my lord?");
+    },
+        function (session, results) {
+        session.dialogData.houseToWageWarWith = results.response;
+        builder.Prompts.number(session, "And how many soldiers shall I conscript for this war?");
+    },
+      function (session, results) {
+        session.dialogData.soldiersInWar = results.response;
+        builder.Prompts.number(session, "Very well. Any dragons?");
+    },
+    function (session, results) {
+        session.dialogData.dragonsInWar = results.response;
+        session.send("Excellent, my Lord.");
+        session.send(`We will be declaring war upon house ${session.dialogData.houseToWageWarWith}, using ${session.dialogData.soldiersInWar} soldiers and ${session.dialogData.dragonsInWar} dragons.`)
+        session.send("I will send a raven immediately, to provision a war room for the forthcoming battle")
+        // TODO:
+        // SEND session.dialogData to flow azure function
+        session.endDialogWithResult(results); 
+        }
+])
+
+
+// POC
+bot.dialog('dice', function (session, args, next) {
+        var queuedMessage = { address: session.message.address, text: session.message.text };
     // add message to queue
     session.sendTyping();
     var queueSvc = azure.createQueueService(process.env.AzureWebJobsStorage);
-    queueSvc.createQueueIfNotExists('bot-queue', function (err, result, response) {
+    queueSvc.createQueueIfNotExists('dice-throw', function (err, result, response) {
         if (!err) {
             // Add the message to the queue
             var queueMessageBuffer = new Buffer(JSON.stringify(queuedMessage)).toString('base64');
-            queueSvc.createMessage('bot-queue', queueMessageBuffer, function (err, result, response) {
+            queueSvc.createMessage('dice-throw', queueMessageBuffer, function (err, result, response) {
                 if (!err) {
-                    // Message inserted
-                    //   session.send('Your message (\'' + session.message.text + '\') has been added to a queue, and it will be sent back to you via a Function');
-                    session.send('Yes my liege - I will summon an army and provision a war room for thee!');
-                } else {
-                    // this should be a log for the dev, not a message to the user
-                    session.send('There was an error inserting your message into queue');
-                }
+                    session.send('Throwing a dice, my liege!');
+                } else {session.send('There was an error inserting your message into queue');}
             });
-        } else {
-            // this should be a log for the dev, not a message to the user
-            session.send('There was an error creating your queue');
-        }
+        } else {session.send('There was an error creating your queue');}
     });
+}).triggerAction({ matches: /^dice$/i, });
 
-});
