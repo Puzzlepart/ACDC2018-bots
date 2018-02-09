5b000db3e76eb79524c23a95db47355ef0810f8b
Kim Damsleth <kim@damsleth.com>
2018-02-09 11:56:25 +0100

    LUIS features 1st commit

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ var documentDbOptions = {
 var docDbClient = new botbuilder_azure.DocumentDbClient(documentDbOptions);
 var cosmosStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, docDbClient);
 
-
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
@@ -45,6 +44,15 @@ var inMemoryStorage = new builder.MemoryBotStorage();
 var bot = new builder.UniversalBot(connector);
 bot.set('storage', inMemoryStorage);
 
+//LUIS SETUP
+// Make sure you add code to validate these fields
+var luisAppId = process.env.LuisAppId;
+var luisAPIKey = process.env.LuisAPIKey;
+var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
+
+const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
+
+// CALLBACK FROM TRIGGERS
 // Intercept trigger event (ActivityTypes.Trigger)
 bot.on('trigger', function (message) {
     // handle message from trigger function
@@ -53,16 +61,14 @@ bot.on('trigger', function (message) {
         .address(queuedMessage.address)
         .text(queuedMessage.text);
     // .text('This is coming from the trigger: ' + queuedMessage.text);
-        bot.send(reply);
+    bot.send(reply);
 });
 
 
-bot.dialog('/',function(session){
+bot.dialog('/', function (session) {
     session.send("Yes, my liege?")
 })
 
-
-//CUSTOM
 
 
 
@@ -78,11 +84,11 @@ bot.dialog('startWar', [
         session.send("Very well...")
         builder.Prompts.text(session, "Upon whom shall we declare war, my lord?");
     },
-        function (session, results) {
+    function (session, results) {
         session.dialogData.houseToWageWarWith = results.response;
         builder.Prompts.number(session, "And how many soldiers shall I conscript for this war?");
     },
-      function (session, results) {
+    function (session, results) {
         session.dialogData.soldiersInWar = results.response;
         builder.Prompts.number(session, "Very well. Any dragons?");
     },
@@ -93,14 +99,14 @@ bot.dialog('startWar', [
         session.send("I will send a raven immediately, to provision a war room for the forthcoming battle")
         // TODO:
         // SEND session.dialogData to flow azure function
-        session.endDialogWithResult(results); 
-        }
+        session.endDialogWithResult(results);
+    }
 ])
 
 
-// POC
+// DICE POC
 bot.dialog('dice', function (session, args, next) {
-        var queuedMessage = { address: session.message.address, text: session.message.text };
+    var queuedMessage = { address: session.message.address, text: session.message.text };
     // add message to queue
     session.sendTyping();
     var queueSvc = azure.createQueueService(process.env.AzureWebJobsStorage);
@@ -111,9 +117,30 @@ bot.dialog('dice', function (session, args, next) {
             queueSvc.createMessage('dice-throw', queueMessageBuffer, function (err, result, response) {
                 if (!err) {
                     session.send('Throwing a dice, my liege!');
-                } else {session.send('There was an error inserting your message into queue');}
+                } else { session.send('There was an error inserting your message into queue'); }
             });
-        } else {session.send('There was an error creating your queue');}
+        } else { session.send('There was an error creating your queue'); }
     });
 }).triggerAction({ matches: /^dice$/i, });
 
+
+// LUIS DIALOG
+var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+var intents = new builder.IntentDialog({ recognizers: [recognizer] })
+    .matches('Greeting', (session) => {
+        session.send('You reached Greeting intent, you said \'%s\'.', session.message.text);
+    })
+    .matches('Help', (session) => {
+        session.send('You reached Help intent, you said \'%s\'.', session.message.text);
+    })
+    .matches('Cancel', (session) => {
+        session.send('You reached Cancel intent, you said \'%s\'.', session.message.text);
+    })
+    /*
+    .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
+    */
+    .onDefault((session) => {
+        session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+    });
+
+bot.dialog('/', intents);    
